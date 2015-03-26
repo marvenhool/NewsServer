d5003c7ca68ee0a9075335a587bd32ed8e953878
WENHAO MA <marvenhool911@hotmail.com>
2015-03-26 11:55:05 +0900

    xml format レスポンスを追加しました

--- NewsServer/app/controllers/news_controller.rb
+++ NewsServer/app/controllers/news_controller.rb
@@ -1,12 +1,26 @@
 class NewsController < ApplicationController
   def index
   end
+
   def all
     @news=News.all
-    render json: @news
+    if params[:format]=="json"
+      render json: @news
+    elsif params[:format]=="xml"
+      render xml: @news
+    else
+      render json: @news
+    end
   end
+
   def id
     @news =News.find(params[:id])
-    render json:@news
+    if params[:format]=="json"
+      render json: @news
+    elsif params[:format]=="xml"
+      render xml: @news
+    else
+      render json: @news
+    end
   end
 end
